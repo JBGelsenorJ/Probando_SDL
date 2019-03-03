c56d330a7cd9417cd98af2869a4adf1e00a90a6c
JBGelsenorJ <javi_belda@live.com>
2019-03-03 17:41:42 +0100

    empezando esta verga

--- ProgramandoConSDL/main.cpp
+++ ProgramandoConSDL/main.cpp
@@ -1,9 +1,16 @@
 #include "../SDL/include/SDL.h"
+#include<stdio.h>;
 #pragma coment(lib, "../SDL/libx86/SDL2.lib");
 #pragma coment(lib, "../SDL/libx86/SDL2main.lib")
-int main() {
+int main(int argc, char* argv[]){
+	printf("hola");
+	/*if (Init() == 0) {
+		return 1;
+	}
+	// Finalize SDL
+	CleanUp();*/
 
-	system("pause");
+	return 0;
 }
 
 
